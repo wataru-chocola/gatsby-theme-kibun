a3de6ca0c52dd66cca1d69cc93d437086f4e870a
Wataru Watanabe <wataru.chocola@gmail.com>
2021-06-19 20:11:01 +0900

    generate sitemap.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -16,18 +17,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    //{
+    //  resolve: `gatsby-plugin-manifest`,
+    //  options: {
+    //    name: `gatsby-starter-default`,
+    //    short_name: `starter`,
+    //    start_url: `/`,
+    //    background_color: `#663399`,
+    //    theme_color: `#663399`,
+    //    display: `minimal-ui`,
+    //    icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //  },
+    //},
     `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-plugin-typegen`,
