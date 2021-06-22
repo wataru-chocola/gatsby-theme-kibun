138d95773f86606ecd08a271253b96164c7c5c8b
Wataru Watanabe <wataru.chocola@gmail.com>
2021-06-22 09:43:05 +0900

    make sidebar responsive

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { AppBar as MUIAppBar, AppBarProps, Toolbar } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import { useStaticQuery, graphql } from "gatsby";
+import MenuIcon from "@material-ui/icons/Menu";
 
 
-const AppBar: React.FC<AppBarProps> = (props) => {
+export const SiteTitle = () => {
   const data = useStaticQuery<GatsbyTypes.SiteTitleQuery>(
     graphql`
     query SiteTitle {
@@ -20,14 +22,38 @@ const AppBar: React.FC<AppBarProps> = (props) => {
   const siteTitle: string = data.site?.siteMetadata?.title || `(no sitename)`
 
   return (
-    <MUIAppBar position="fixed" {...props}>
+    <Typography variant="h6" noWrap>
+      {siteTitle}
+    </Typography>
+  );
+};
+
+interface MenuButtonProps {
+  onClick: () => void;
+  className: string;
+}
+
+export const MenuButton = ({ onClick, className }: MenuButtonProps) => {
+  return (
+  <IconButton
+    color="inherit"
+    aria-label="open drawer"
+    edge="start"
+    onClick={onClick}
+    className={className}
+  >
+    <MenuIcon />
+  </IconButton>
+  );
+};
+
+export const AppBar: React.FC<AppBarProps> = (props) => {
+  const { children, ...passThroughProps } = props;
+  return (
+    <MUIAppBar position="fixed" {...passThroughProps}>
       <Toolbar>
-        <Typography variant="h6" noWrap>
-          {siteTitle}
-        </Typography>
+        {props.children}
       </Toolbar>
     </MUIAppBar>
   );
 };
-
-export default AppBar;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,11 +3,12 @@ import { CssBaseline, Toolbar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import { Container } from "@material-ui/core";
+import { Hidden } from "@material-ui/core";
 
 import Seo from "../components/seo";
 
-import AppBar from "../components/appbar";
-import SideBarDrawer from "../components/sidebar";
+import { AppBar, SiteTitle, MenuButton } from "../components/appbar";
+import { SideBarDrawer, MobileDrawer, DrawerContent } from "../components/sidebar";
 import PathBreadcrumbs from "../components/breadcrumbs";
 
 
@@ -20,9 +21,17 @@ const useStyles = makeStyles((theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: "none",
+    },
+  },
   drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
   },
   drawerPaper: {
     width: drawerWidth,
@@ -34,19 +43,48 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-const Layout: React.FC<{ pageTitle: string }> = ({ pageTitle, children }) => {
+interface LayoutProps {
+  pageTitle: string;
+  window?: () => Window;
+}
+
+
+const Layout: React.FC<LayoutProps> = ({ window, pageTitle, children }) => {
   const classes = useStyles();
+  const [mobileDrawerOpen, setMobileDrawerOpen] = React.useState(false);
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  const handleDrawerToggle = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
       <Seo title={pageTitle} />
-      <AppBar className={classes.appBar} />
+      <AppBar className={classes.appBar} >
+        <MenuButton className={classes.menuButton} onClick={handleDrawerToggle}/>
+        <SiteTitle />
+      </AppBar>
+
       <nav aria-label="sidemenu">
-        <SideBarDrawer className={classes.drawer} classes={{
-          paper: classes.drawerPaper,
-        }}/>
+        <Hidden smUp implementation="css">
+          <MobileDrawer container={container}
+           open={mobileDrawerOpen} onClose={handleDrawerToggle}
+          >
+            <DrawerContent />
+          </MobileDrawer>
+        </Hidden>
+
+        <Hidden xsDown implementation="css">
+          <SideBarDrawer className={classes.drawer} classes={{
+            paper: classes.drawerPaper,
+          }}>
+            <DrawerContent />
+          </SideBarDrawer>
+        </Hidden>
       </nav>
+
       <main className={classes.content}>
         <Toolbar />
         <Container maxWidth="md">
