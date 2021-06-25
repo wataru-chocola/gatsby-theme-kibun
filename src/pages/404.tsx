74a2256f545e2c32c01eab9e820f7c3de0bafb2d
Wataru Watanabe <wataru.chocola@gmail.com>
2021-06-25 21:15:51 +0900

    fix type

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 
 import Layout from '../components/layout';
 
-const NotFoundPage = () => (
+const NotFoundPage: React.VFC = () => (
   <Layout pageTitle="404: Not found">
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
