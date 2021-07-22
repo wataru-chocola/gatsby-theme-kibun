ea26e32cce0486249a69b57377ce0e6ef31d3498
Wataru Watanabe <wataru.chocola@gmail.com>
2021-07-22 14:15:10 +0900

    replace image wrapper paragraph with div

--- src/utils/markdownParser.ts
+++ src/utils/markdownParser.ts
@@ -1,6 +1,9 @@
 import React from 'react';
 import unified from 'unified';
 import { Element } from 'hast';
+import { Paragraph } from 'mdast';
+import * as mdast2hast from 'mdast-util-to-hast';
+import * as all from 'mdast-util-to-hast/lib/all';
 import remarkParser from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeRaw from 'rehype-raw';
@@ -10,9 +13,20 @@ import hastToc from './hastToc';
 import * as matter from 'gray-matter';
 import { renderAst, ImageDataCollection } from './rehype';
 
+const mdastParagraph2hast: mdast2hast.Handler = (h, tmp_node) => {
+  const node = tmp_node as Paragraph;
+  if (node.children.length === 1 && node.children[0].type === 'image') {
+    return h(node, 'div', all(h, node));
+  }
+  return h(node, 'p', all(h, node));
+};
+
 const markdownHastProcessor = unified()
   .use(remarkParser)
-  .use(remarkRehype, { allowDangerousHtml: true })
+  .use(remarkRehype, undefined, {
+    allowDangerousHtml: true,
+    handlers: { paragraph: mdastParagraph2hast },
+  })
   .use(rehypeRaw)
   .use(codeRefractor, {
     aliases: {
@@ -33,13 +47,13 @@ export function md2react(
   imgdataCollection: ImageDataCollection = {},
 ): React.ReactElement | null {
   const mdast = markdownHastProcessor.parse(md);
-  const hast = (markdownHastProcessor.runSync(mdast) as unknown) as Element;
+  const hast = markdownHastProcessor.runSync(mdast) as unknown as Element;
   return renderAst(hast, pageSlug, imgdataCollection);
 }
 
 export function md2toc(md: string): React.ReactElement | null {
   const mdast = markdownHastProcessor.parse(md);
-  const hast = (markdownHastProcessor.runSync(mdast) as unknown) as Element;
+  const hast = markdownHastProcessor.runSync(mdast) as unknown as Element;
   const toc = hastToc(hast);
   if (toc == null) {
     return null;
