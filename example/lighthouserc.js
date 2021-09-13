9b0e3d080562f6c2977def60a957605838f091f8
Wataru Watanabe <wataru.chocola@gmail.com>
2021-09-13 13:45:54 +0900

    add lighthouse-ci

---
+++ example/lighthouserc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:9000/'],
+      startServerCommand: 'npm run serve',
+      startServerReadyPattern: "You can now view",
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+    },
+    //upload: {
+    //  target: 'temporary-public-storage',
+    //},
+  },
+};
