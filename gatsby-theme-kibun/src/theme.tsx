6bc96a385c3decd3ff2faad4d56cec8567c9a936
Wataru Watanabe <wataru.chocola@gmail.com>
2021-11-08 17:53:49 +0900

    re-enable locale

--- gatsby-theme-kibun/src/theme.tsx
+++ gatsby-theme-kibun/src/theme.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createTheme } from '@mui/material/styles';
-//import { jaJP } from '@mui/material/locale';
+import { jaJP } from '@mui/material/locale';
 import { ThemeProvider, Theme, StyledEngineProvider } from '@mui/material/styles';
 import { indigo } from '@mui/material/colors';
 
@@ -14,58 +14,61 @@ const fontFamily = [
   'sans-serif',
 ];
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: indigo[700],
+const theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: indigo[700],
+      },
+    },
+    typography: {
+      fontFamily: fontFamily.join(','),
+      h1: {
+        fontSize: '2rem',
+        fontWeight: 500,
+        lineHeight: 1.75,
+        letterSpacing: '-0.035em',
+      },
+      h2: {
+        fontSize: '1.65rem',
+        fontWeight: 500,
+        lineHeight: 1.6,
+        letterSpacing: '-0.03em',
+      },
+      h3: {
+        fontSize: '1.5rem',
+        fontWeight: 500,
+        lineHeight: 1.5,
+        letterSpacing: '-0.025em',
+      },
+      h4: {
+        fontSize: '1.25rem',
+        lineHeight: 1.5,
+        letterSpacing: '-0.02em',
+      },
+      h5: {
+        fontSize: '1.15rem',
+        lineHeight: 1.5,
+        letterSpacing: '-0.02em',
+      },
+      h6: {
+        fontSize: '1.05rem',
+        lineHeight: 1.5,
+        letterSpacing: '-0.02em',
+      },
+      body1: {
+        lineHeight: 1.7,
+        letterSpacing: '0.05em',
+      },
+      caption: {
+        fontSize: '0.85rem',
+        lineHeight: 1.75,
+        letterSpacing: '0.075em',
+      },
     },
   },
-  typography: {
-    fontFamily: fontFamily.join(','),
-    h1: {
-      fontSize: '2rem',
-      fontWeight: 500,
-      lineHeight: 1.75,
-      letterSpacing: '-0.035em',
-    },
-    h2: {
-      fontSize: '1.65rem',
-      fontWeight: 500,
-      lineHeight: 1.6,
-      letterSpacing: '-0.03em',
-    },
-    h3: {
-      fontSize: '1.5rem',
-      fontWeight: 500,
-      lineHeight: 1.5,
-      letterSpacing: '-0.025em',
-    },
-    h4: {
-      fontSize: '1.25rem',
-      lineHeight: 1.5,
-      letterSpacing: '-0.02em',
-    },
-    h5: {
-      fontSize: '1.15rem',
-      lineHeight: 1.5,
-      letterSpacing: '-0.02em',
-    },
-    h6: {
-      fontSize: '1.05rem',
-      lineHeight: 1.5,
-      letterSpacing: '-0.02em',
-    },
-    body1: {
-      lineHeight: 1.7,
-      letterSpacing: '0.05em',
-    },
-    caption: {
-      fontSize: '0.85rem',
-      lineHeight: 1.75,
-      letterSpacing: '0.075em',
-    },
-  },
-});
+  jaJP,
+);
 
 declare module '@mui/styles/defaultTheme' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
