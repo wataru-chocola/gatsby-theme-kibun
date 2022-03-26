7034d367fd9e0147e63edc4c90619bae37befd4c
Wataru Watanabe <wataru.chocola@gmail.com>
2022-03-26 21:22:43 +0900

    make code for main container into a new file

---
+++ gatsby-theme-kibun/src/components/contentContainer/contentContainer.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Toolbar } from '@mui/material';
+import { Box } from '@mui/material';
+import { Paper } from '@mui/material';
+
+import { EditButton } from './editButton';
+import { Footer } from '../footer';
+import ErrorBoundary from '../utils/errorboundary';
+
+export type ContentContainerProps = {
+  footer: React.ReactElement;
+};
+
+export const ContentContainer: React.FC<ContentContainerProps> = (props) => {
+  return (
+    <Box
+      component="main"
+      sx={{
+        minWidth: { xs: '320px' },
+        maxWidth: { xs: '100%', sm: '900px', md: '900px' },
+        width: '100%',
+        position: 'relative',
+      }}
+    >
+      <Toolbar />
+
+      <ErrorBoundary fallback={<h1>Error: Something wrong happened (&gt;&lt;)</h1>}>
+        <Box
+          sx={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            pointerEvents: 'none',
+            '& > *': {
+              pointerEvents: 'auto',
+            },
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <EditButton />
+        </Box>
+        <Paper>
+          {props.children}
+          <Box mt={8}>{props.footer}</Box>
+        </Paper>
+      </ErrorBoundary>
+    </Box>
+  );
+};
+ContentContainer.displayName = 'ContentContainer';
--- gatsby-theme-kibun/src/components/innerLayout.tsx
+++ gatsby-theme-kibun/src/components/innerLayout.tsx
@@ -1,15 +1,13 @@
 import React, { useCallback } from 'react';
 import { Toolbar } from '@mui/material';
 import { Box } from '@mui/material';
-import { Paper } from '@mui/material';
 
 import { HeaderBar } from './headerBar';
 import { SectionBar } from './sectionBar';
 import { Attachments } from './attachments';
 import { SnackMessage } from './utils/snackMessage';
-import { EditButton } from './editButton';
+import { ContentContainer } from './contentContainer';
 import { Footer } from './footer';
-import ErrorBoundary from './utils/errorboundary';
 
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import 'overlayscrollbars/css/OverlayScrollbars.css';
@@ -51,41 +49,7 @@ const InnerLayout: React.FC<InnerLayoutProps> = ({ pageTitle, children }) => {
         </nav>
 
         <Box sx={{ display: 'flex', justifyContent: 'center', flexGrow: 1, width: '100%' }}>
-          <Box
-            component="main"
-            sx={{
-              minWidth: { xs: '320px' },
-              maxWidth: { xs: '100%', sm: '900px', md: '900px' },
-              width: '100%',
-              position: 'relative',
-            }}
-          >
-            <Toolbar />
-
-            <ErrorBoundary fallback={<h1>Error: Something wrong happened (&gt;&lt;)</h1>}>
-              <Box
-                sx={{
-                  position: 'absolute',
-                  width: '100%',
-                  height: '100%',
-                  pointerEvents: 'none',
-                  '& > *': {
-                    pointerEvents: 'auto',
-                  },
-                  display: 'flex',
-                  justifyContent: 'flex-end',
-                }}
-              >
-                <EditButton />
-              </Box>
-              <Paper>
-                {children}
-                <Box mt={8}>
-                  <Footer />
-                </Box>
-              </Paper>
-            </ErrorBoundary>
-          </Box>
+          <ContentContainer footer={<Footer />}>{children}</ContentContainer>
           <Box width="350px" sx={{ display: { xs: 'none', lg: 'block' }, flexShrink: 0 }}>
             <Box
               height="100%"
