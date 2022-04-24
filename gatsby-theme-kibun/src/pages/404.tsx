897729f10eb9f786d143615e7c2b7c6910e79f0d
Wataru Watanabe <wataru.chocola@gmail.com>
2022-04-24 10:20:11 +0900

    fix: broken 404 page

--- gatsby-theme-kibun/src/pages/404.tsx
+++ gatsby-theme-kibun/src/pages/404.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
-import Layout from '../components/layout';
+import { Typography, Box, MenuProps } from '@mui/material';
 
-const NotFoundPage: React.VFC = () => (
-  <Layout pageTitle="404: Not found">
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+import Layout, { useLayoutControl } from '../components/layout';
+import { HeaderContent } from '../components/header';
+import { SectionMenu } from '../components/sectionMenu';
+import { Attachments } from '../components/attachments';
+import { ActionMenu } from '../components/header/actionMenu';
+
+const NotFoundPage: React.VFC = () => {
+  const title = '404: Not Found';
+  const layoutControl = useLayoutControl();
+  const menuRender = React.useCallback((props: MenuProps) => {
+    return <ActionMenu {...props} />;
+  }, []);
+
+  return (
+    <Layout
+      pageTitle={title}
+      headerContent={<HeaderContent pageTitle={title} menuRender={menuRender} />}
+      sidebarContent={<SectionMenu />}
+      rightPanelContent={<Attachments />}
+      control={layoutControl}
+    >
+      <Box my={3}>
+        <Typography variant="h1">404: Not Found</Typography>
+        <Typography variant="body1">
+          You just hit a route that doesn&#39;t exist... the sadness.
+        </Typography>
+      </Box>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
